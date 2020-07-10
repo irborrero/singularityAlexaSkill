3c2d7e4821a4e12867c215ae55a42beb293d4fa7
Isabella Ramirez <rmiisa@amazon.com>
2020-07-10 19:32:25 +0100

    setup

--- lib/alexa_skill-stack.ts
+++ lib/alexa_skill-stack.ts
@@ -1,9 +1,14 @@
-import * as cdk from '@aws-cdk/core';
+import cdk = require('@aws-cdk/core');
+import {Code, Function, Runtime} from "@aws-cdk/aws-lambda"
 
 export class AlexaSkillStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambda = new Function(this, "lambda", {
+      runtime: Runtime.NODEJS_10_X,    // execution environment
+      code: Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
   }
 }
