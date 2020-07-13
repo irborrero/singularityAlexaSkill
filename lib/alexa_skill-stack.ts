50531d96530f70aeb4074d32ff1a28a44fda1614
Isabella Ramirez <rmiisa@amazon.com>
2020-07-13 16:08:45 +0100

    added alexa index.js

--- lib/alexa_skill-stack.ts
+++ lib/alexa_skill-stack.ts
@@ -8,7 +8,7 @@ export class AlexaSkillStack extends cdk.Stack {
     const lambda = new Function(this, "lambda", {
       runtime: Runtime.NODEJS_10_X,    // execution environment
       code: Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
+      handler: 'index.handler'                // file is "hello", function is "handler"
     });
   }
 }
